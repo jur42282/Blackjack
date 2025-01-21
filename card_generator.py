3378bd7a28b442da97a81beb321e0215b5c2a842
jur42282 <jur42282@spseol.cz>
2025-01-21 16:21:57 +0100

    Multiplayer

--- card_generator.py
+++
@@ -1,8 +0,0 @@
-class Card_generator:
-    def generate_cards(self):
-        for _ in range(4):
-            for suits in ['♠', '♥', '♣', '♦']:
-                for values in range(2, 11):
-                    yield f'{suits}{values}'
-                for values in ['J', 'Q', 'K', 'A']:
-                    yield f'{suits}{values}'
--- main.py
+++ main.py
@@ -14,9 +14,6 @@ class Player:
     def hit(self, deck):
         self.hand.append(deck[0])
         del deck[0]
-    
-    def stand(self):
-        pass
 
     def __str__(self):
         return f"Hráč: {self.name}\nKarty: {self.hand}"
@@ -37,65 +34,109 @@ class Deck:
         shuffle(self.deck)
 
 def main():
-    print("Vítej v BlackJacku!\nZadej své jméno:")
-    player = Player(input())
-    dealer = Player("Dealer")
-    player.generate_hand(deck.deck)
-    dealer.generate_hand(deck.deck)
-    print(f"Karta dealera: {dealer.hand[0]}")
+    print("Vítej v BlackJacku!\nZadej počet hráčů (Max 6):")
+    try:
+        p_count = int(input())
+    except ValueError:
+        print("Zadej číslo PYČO!")
+        return
+    if p_count < 1 or p_count > 6:
+        print("Zadej číslo mezi 1 a 6")
+        return
 
-    while True: # Hráčova smyčka 
+    players = [Player(f'Hráč {i+1}') for i in range(p_count)]
+    for player in players:
+        player.generate_hand(deck.deck)
+        name = input(f"Zadej své jméno: ")
+        if name != "":
+            player.name = name
+        else:
+            player.name = f"Hráč {players.index(player)+1}"
         print(player)
-        player.hand_value = sum([card_value(card) for card in player.hand])
-        if player.hand_value > 21:
-            print("Přepálil jsi!")
-            print(dealer)
-            exit()
-        if player.hand_value <= 21:
-            print("Hit/stand")
-            player_choice = input()
-            if player_choice == "hit":
-                player.hit(deck.deck)
-            elif player_choice == "stand":
+        sleep(1)
+        print("")
+        
+    dealer = Player('Dealer')
+    dealer.generate_hand(deck.deck)
+    print(f"{dealer.name}\nKarty: {dealer.hand[0]}, ?")
+    sleep(3)
+    print("")
+    # Hráči
+    for player in players:
+        while True:
+            player.hand_value = sum([card_value(card) for card in player.hand]) # Sečte hodnoty karet v listu
+            print(player)
+            if player.hand_value > 21:
+                print(f"{player.name} přestřelil!")
+                sleep(1)
+                print("")
                 break
-    while True: # Dealerova smyčka
-        print(dealer)
+            sleep(0.5)
+            print(f"{player.name} hit / stand?")
+            action = input().lower()
+            if action == 'hit':
+                player.hit(deck.deck)
+                print(player)
+            elif action == 'stand':
+                sleep(1)
+                print("")
+                break
+            else:
+                print("Zadej hit nebo stand")
+                continue
+    
+   # Dealer
+    while True:
         dealer.hand_value = sum([card_value(card) for card in dealer.hand])
-        sleep(1)
-        print("Dealer hraje...")
-        sleep(1)
+        print(dealer)
         if dealer.hand_value > 21:
-            print("Dealer přepálil!")
+            print("Dealer přestřelil!")
+            sleep(1)
+            print("")
+            break
+        if dealer.hand_value >= 17:
+            print("Dealer stojí")
+            sleep(1)
+            print("")
             break
         if dealer.hand_value < 17:
             dealer.hit(deck.deck)
-        if dealer.hand_value == 17:
-            print("Dealer stojí")
-            break
-        if dealer.hand_value > 17:
-            print("Dealer stojí")
-            break
-    # Porovnání
-    # print(player.hand_value)
-    # print(dealer.hand_value)
-    if player.hand_value > 21:
-        print("Prohrál jsi!")
-    elif dealer.hand_value > 21:
-        print("Vyhrál jsi!")
-    elif player.hand_value > dealer.hand_value:
-        print("Vyhrál jsi!")
-    elif player.hand_value == dealer.hand_value:
-        print("Remíza!")
-    else:
-        print("Prohrál jsi!")
+            print(dealer)
+            sleep(1)
+            continue
 
-        
-    
+    # Vyhodnocení
+    winners = 0
+    for player in players:
+        if player.hand_value > 21:
+            continue
+        if dealer.hand_value > 21 and player.hand_value <= 21:
+            print(f"{player.name} vyhrál!")
+            winners += 1
+            sleep(1)
+            print("")
+        elif player.hand_value > dealer.hand_value:
+            print(f"{player.name} vyhrál!")
+            winners += 1
+            sleep(1)
+            print("")
+        elif player.hand_value == dealer.hand_value:
+            print(f"{player.name} push")
+            winners += 1
+            sleep(1)
+            print("")
+    if winners == 0:
+        print("Dealer vyhrál!")
+        sleep(1)
+        print("")
+
+            
+
 def card_value(card):
     if card[1].isdigit(): # metoda checkuje jestli je hodnota číslo, vrací T/F 
         return int(card[1:])
-    if card[1] == 'A': # Eso = 11
-        return 11
+    if card[1] == 'A': # Eso = 11 
+            return 11
     return 10 # J, Q, K = 10
 
 deck= Deck()
